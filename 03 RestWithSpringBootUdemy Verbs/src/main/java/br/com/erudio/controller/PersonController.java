ab2e4d06454aa315430d66d670cf6d41dc710fc9
Paulo Viana <pauloc.fviana@hotmail.com>
2021-03-31 15:54:48 -0300

    versionamento de api

--- 03 RestWithSpringBootUdemy Verbs/src/main/java/br/com/erudio/controller/PersonController.java
+++ 03 RestWithSpringBootUdemy Verbs/src/main/java/br/com/erudio/controller/PersonController.java
@@ -13,7 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.erudio.model.Person;
+import br.com.erudio.data.vo.PersonVO;
+import br.com.erudio.data.vo.v2.PersonVOV2;
 import br.com.erudio.services.PersonServices;
 
 @RestController
@@ -24,23 +25,28 @@ public class PersonController {
 	private PersonServices service;
 	
 	@GetMapping
-	public List<Person> findAll() {
+	public List<PersonVO> findAll() {
 		return service.findAll();
 	}	
 	
 	@GetMapping("/{id}")
-	public Person findById(@PathVariable("id") Long id) {
+	public PersonVO findById(@PathVariable("id") Long id) {
 		return service.findById(id);
 	}	
 	
 	@PostMapping
-	public Person create(@RequestBody Person person) {
+	public PersonVO create(@RequestBody PersonVO person) {
 		return service.create(person);
 	}
 	
+	@PostMapping("/v2")
+	public PersonVOV2 createV2(@RequestBody PersonVOV2 person) {
+		return service.createV2(person);
+	}
+	
 	@PutMapping("/{id}")
-	public Person update(@RequestBody Person person) {
-		return service.update(person);
+	public PersonVO update(@RequestBody PersonVO person) {
+		return service.update(person); 
 	}	
 	
 	@DeleteMapping("/{id}")
--- 03 RestWithSpringBootUdemy Verbs/src/main/java/br/com/erudio/services/PersonServices.java
+++ 03 RestWithSpringBootUdemy Verbs/src/main/java/br/com/erudio/services/PersonServices.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import br.com.erudio.converter.DozerConverter;
+import br.com.erudio.converter.custom.PersonConverter;
+import br.com.erudio.data.model.Person;
+import br.com.erudio.data.vo.PersonVO;
+import br.com.erudio.data.vo.v2.PersonVOV2;
 import br.com.erudio.exception.ResourceNotFoundException;
-import br.com.erudio.model.Person;
 import br.com.erudio.request.repository.PersonRepository;
 
 @Service
@@ -13,17 +17,32 @@ public class PersonServices {
 	
 	private final PersonRepository repository;
 	
-	public PersonServices(PersonRepository repository) {
+	private final PersonConverter converter;
+
+	public PersonServices(PersonRepository repository, PersonConverter converter) {
 		this.repository = repository;
+		this.converter = converter;
 	}
 
-	public Person create(Person person) {
-		return repository.save(person);
+	public PersonVO create(PersonVO person) {
+		var entity = DozerConverter.parseObject(person, Person.class);
+		
+		var vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+		
+		return vo;
 	}
 	
-	public Person update(Person person) {
+	public PersonVOV2 createV2(PersonVOV2 person) {
+		var entity = converter.convertVoToEntity(person);
 		
-		Person entity = repository.findById(person.getId())
+		var vo = converter.convertEntityToVo(repository.save(entity));
+		
+		return vo;
+	}
+	
+	public PersonVO update(PersonVO person) {
+		
+		var entity = repository.findById(person.getId())
 				.orElseThrow(() -> new ResourceNotFoundException("No records found for this id."));
 		
 		entity.setFirstName(person.getFirstName());
@@ -31,7 +50,8 @@ public class PersonServices {
 		entity.setAddress(person.getAddress());
 		entity.setGender(person.getGender());
 		
-		return repository.save(entity);
+		var vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+		return vo;
 	}	
 	
 	public void delete(Long id) {
@@ -42,12 +62,16 @@ public class PersonServices {
 		repository.delete(entity);
 	}
 	
-	public Person findById(Long id) {
-		return repository.findById(id)
+	public PersonVO findById(Long id) {
+		
+		var entity = repository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("No records found for this id."));
+		
+		return DozerConverter.parseObject(entity, PersonVO.class);
 	}
 	
-	public List<Person> findAll() {
-		return repository.findAll();
+	public List<PersonVO> findAll() {
+		
+		return DozerConverter.parseListObjects(repository.findAll(), PersonVO.class);
 	}
 }
